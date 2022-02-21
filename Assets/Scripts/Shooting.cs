8e45658052ebeb29aa7d336afbd479e0114615f7
SINdivCOS <99553613+SINdivCOS@users.noreply.github.com>
2022-02-22 00:10:50 +0700

    Add 2D Camera
    - fixed Bullet dir
    - fixed GetButtonDown Bullet
    - add Bullet Value
    - add Camera follow

--- Assets/Scripts/Shooting.cs
+++ Assets/Scripts/Shooting.cs
@@ -7,14 +7,20 @@ public class Shooting : MonoBehaviour
     public Transform BulletOut;
     public GameObject bulletPrefab;
 
+    public int bulletValue = 10;
     public float bulletForce = 20f;
     
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetButton("Fire1"))
+        if (Input.GetButtonDown("Fire1"))
         {
-            Shoot();
+            if (bulletValue > 0)
+            {
+                Shoot();
+                bulletValue--;
+                Debug.Log(bulletValue);
+            }
         }
     }
 
@@ -22,6 +28,6 @@ public class Shooting : MonoBehaviour
     {
         GameObject bullet = Instantiate(bulletPrefab, BulletOut.position, BulletOut.rotation);
         Rigidbody2D _rigidbody2D = bullet.GetComponent<Rigidbody2D>();
-        _rigidbody2D.AddForce(BulletOut.up * bulletForce, ForceMode2D.Impulse);
+        _rigidbody2D.AddForce(BulletOut.right * bulletForce, ForceMode2D.Impulse);
     }
 }
